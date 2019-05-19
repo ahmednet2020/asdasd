09ba9eac83cd479e2b073217ba3fb95e37d9093c
ahmednet <ahmednet03@gmail.com>
2019-05-20 00:54:53 +0200

    nav-day-2

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,6 +1,14 @@
 import * as jQuery from 'jquery'
 jQuery(($)=>{
-    const nav = $("nav") as JQuery<HTMLElement>
-    console.dir(nav)
-    console.dir($)
+    const toggleNav = $(".toggle-nav") as JQuery<HTMLElement>
+    const closeNav = $(".close-icon") as JQuery<HTMLElement>
+    const nav = $(".nav-content") as JQuery<HTMLElement>
+    toggleNav.on("click", (e)=>{
+        e.preventDefault()
+        nav.addClass("show")
+    })
+    closeNav.on("click", (e) => {
+        e.preventDefault()
+        nav.removeClass("show")
+    })
 })
